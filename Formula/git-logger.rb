12a0eeea9aa4927e05f5658d07c6b13d8bcec607
Giacomo Rossetto <jackyman_cs4@live.it>
2019-07-21 15:51:49 +0200

    Update git-logger patch
    Remove grep aliasing

--- Formula/git-logger.rb
+++ Formula/git-logger.rb
@@ -5,6 +5,11 @@ class GitLogger < Formula
     version "0.99.0-1"
     sha256 "8b900e78599230305e943488330d949a5a1c76b55f478225fe8bef04654723e8"
 
+    patch do
+      url "https://raw.githubusercontent.com/Jackymancs4/homebrew-tap/feature/git-logger-formula/patches/git-logger.patch"
+      sha256 "9a3b75fd6fe7942fd48d36226c24d10576e9589d9cf1a5382f6569f0a2369f7a"
+    end
+
     def install
 
       # Create extension-less executable
